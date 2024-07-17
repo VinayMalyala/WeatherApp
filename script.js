7b764bc3e0bf7d63a7f750eddea8c696efeaf160
Vinay Malyala <srivinay1432@gmail.com>
2024-07-17 14:43:17 +0530

    added functionality

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '7dc9ad880cmsh13aae9548b0d47ap167199jsnceb8f8bde83e',
-		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
-};
+let apiKey = "c256adae061c249711dd124b57f9cc61";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
 
-try {
-	const response = fetch(url, options);
-	const result = response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+
+async function checkWeather(city){
+	const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+	var data = await response.json();
+	document.querySelector(".city").innerHTML = data.name;
+	document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °c";
+	document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+	document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+	document.querySelector(".weather-icon").src = `images/${(data.weather[0].main).toLowerCase()}.png`
 }
+
+
+searchBtn.addEventListener("click", () => {
+	const city = searchBox.value;
+	checkWeather(city);
+});
+
+checkWeather("Visakhapatnam");
